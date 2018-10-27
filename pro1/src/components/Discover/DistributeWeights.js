087a60572dd0322c31d584c6eb17b6cf5f92c4ac
KatherineJY <kellymayliu@163.com>
2018-10-27 19:47:44 +0800

    distributeWeights

--- pro1/src/components/Discover/DistributeWeights.js
+++ pro1/src/components/Discover/DistributeWeights.js
@@ -14,6 +14,7 @@ import {
   Facet,
   Util
 } from "bizcharts";
+import { Slider, InputNumber, Row, Col, Icon } from "antd";
 import DataSet from "@antv/data-set";
 
 import "../../assets/css/distribute.css";
@@ -23,30 +24,77 @@ class DistributeWeights extends Component {
     super(props);
     this.state = {
       domain: "",
+      // adjustMsg: "OK",
+      // willNext: true,
       data: [
         {
           item: "IT",
-          value: 0.2
+          value: 0.2,
         },
         {
           item: "FINANCE",
-          value: 0.2
+          value: 0.2,
         },
         {
           item: "AGRICULTURE",
-          value: 0.2
+          value: 0.2,
         },
         {
           item: "REAL STATE",
-          value: 0.2
+          value: 0.2,
         },
         {
           item: "MANUFACTURE",
-          value: 0.2
+          value: 0.2,
         }
       ]
     };
   }
+
+  componentDidMount(){
+    // let tempData = this.state.data;
+    // tempData.map((item)=>{
+    //   return {
+    //     item: item.item,
+    //     value: item.value,
+    //   }
+    // })
+  }
+
+  onChange = (key,value) => {
+    if (isNaN(value) || value>1 || value<0) {
+      return;
+    }
+    // console.log(value);
+    // value = value.toFixed(2);
+    // console.log(value);
+    let tempData = this.state.data;
+    let sum = 0;
+    console.log(key);
+    for(let i=0;i<key;i++)
+      sum = sum + tempData[i].value;
+    if( sum+value > 1.0 )
+      return;
+    else {
+      tempData[key].value = value;
+      sum = sum+value;
+    }
+    for(let i=key+1;i<tempData.length-1;i++){
+      if( sum+tempData[i].value > 1.0 ){
+        tempData[i].value = sum<1.0?1.0-sum:0;        
+        sum = 1.0;
+      }
+      else sum = sum+tempData[i].value;
+    }
+    tempData[tempData.length-1].value = 1-sum;
+    for(let i=0;i<tempData.length;i++)
+      tempData[i].value = parseFloat(tempData[i].value.toFixed(2));
+    this.setState({
+      data:tempData
+    })
+  }
+  
+   
 
   render() {
     const { DataView } = DataSet;
@@ -61,66 +109,97 @@ class DistributeWeights extends Component {
     const cols = {
       percent: {
         formatter: val => {
-          val = val * 100 + "%";
+          val = (val * 100).toFixed(2) + "%";
           return val;
         }
       }
     };
     return (
-      <div className="pieChartContainer">
-        <Chart
-          height={window.innerHeight*0.5}
-          data={dv}
-          scale={cols}
-          padding={[0, 0, 0, 0]}
-          forceFit
-        >
-          <Coord type={"theta"} radius={0.75} innerRadius={0.6} />
-          <Axis name="percent" />
-          <Legend
-            position="right"
-            offsetY={-window.innerHeight / 2 + 120}
-            offsetX={-100}
-          />
-          <Tooltip
-            showTitle={false}
-            itemTpl="<li><span style=&quot;background-color:{color};&quot; class=&quot;g2-tooltip-marker&quot;></span>{name}: {value}</li>"
-          />
-          {/* <Guide>
-            <Html
-              position={["50%", "50%"]}
-              html="<p>Adjust Distribution Weight of Sectors</p>"
-              alignX="middle"
-              alignY="middle"
-            />
-          </Guide> */}
-          <Geom
-            type="intervalStack"
-            position="percent"
-            color="item"
-            tooltip={[
-              "item*percent",
-              (item, percent) => {
-                percent = percent * 100 + "%";
-                return {
-                  name: item,
-                  value: percent
-                };
-              }
-            ]}
-            style={{
-              lineWidth: 1,
-              stroke: "#fff"
-            }}
+      <div className="distribute-container">
+        <div className="pieChartContainer">
+          <Chart
+            height={window.innerHeight * 0.5}
+            data={dv}
+            scale={cols}
+            padding={[0, 0, 0, 0]}
+            forceFit
           >
-            <Label
-              content="percent"
-              formatter={(val, item) => {
-                return item.point.item + ": " + val;
-              }}
+            <Coord type={"theta"} radius={0.75} innerRadius={0.6} />
+            <Axis name="percent" />
+            <Legend
+              position="right"
+              offsetY={-window.innerHeight / 2 + 120}
+              offsetX={-150}
             />
-          </Geom>
-        </Chart>
+            <Tooltip
+              showTitle={false}
+              itemTpl="<li><span style=&quot;background-color:{color};&quot; class=&quot;g2-tooltip-marker&quot;></span>{name}: {value}</li>"
+            />
+            <Geom
+              type="intervalStack"
+              position="percent"
+              color="item"
+              tooltip={[
+                "item*percent",
+                (item, percent) => {
+                  percent = (percent * 100).toFixed(2) + "%";
+                  return {
+                    name: item,
+                    value: percent
+                  };
+                }
+              ]}
+              style={{
+                lineWidth: 1,
+                stroke: "#fff"
+              }}
+            >
+              <Label
+                content="percent"
+                formatter={(val, item) => {
+                  return item.point.item + ": " + val;
+                }}
+              />
+            </Geom>
+          </Chart>
+        </div>
+        <div className="adjustContainer">
+          {this.state.data.map((item, key) => {
+            return (
+              <Row type="flex" align="middle" key={key}>
+                <Col span={6}>{item.item}</Col>
+                <Col span={8}>
+                  <Slider
+                    min={0}
+                    max={1}
+                    onChange={this.onChange.bind(this,key)}
+                    value={typeof item.value === "number" ? item.value : 0}
+                    step={0.01}
+                  />
+                </Col>
+                <Col span={6}>
+                  <InputNumber
+                    min={0}
+                    max={1}
+                    style={{ marginLeft: 16 }}
+                    step={0.01}
+                    value={item.value}
+                    onChange={this.onChange.bind(this,key)}
+                  />
+                </Col>
+              </Row>
+            );
+          })}
+
+          {/* <p>
+            {this.state.willNext ? (
+              <Icon type="check-circle" theme="twoTone" />
+            ) : (
+              <Icon type="warning" theme="twoTone" />
+            )}
+            {this.state.adjustMsg}
+          </p> */}
+        </div>
       </div>
     );
   }
--- pro1/src/components/Discover/SelectSectors.js
+++ pro1/src/components/Discover/SelectSectors.js
@@ -1,84 +1,109 @@
-import React, { Component } from 'react';
-import { Row, Col, Button } from 'antd';
+import React, { Component } from "react";
+import { Row, Col, Button } from "antd";
 
-import 'antd/dist/antd.css'
-import '../../assets/css/sector.css'
+import "antd/dist/antd.css";
+import "../../assets/css/sector.css";
 
+const axios = require("axios");
 
 class SelectSectors extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            domain: "",
-            data: [
-                {
-                    title: "IT",
-                    esg: 90,
-                    checked:false,
-                },
-                {
-                    title: "FINANCE",
-                    esg: 85,
-                    checked:false,
-                },
-                {
-                    title: "AGRICULTURE",
-                    esg: 70,
-                    checked:false,
-                },
-                {
-                    title: "REAL STATE",
-                    esg: 60,
-                    checked:false,
-                },
-                {
-                    title: "MANUFACTURE",
-                    esg: 77,
-                    checked:false,
-                },
-                
-            ]
+  constructor(props) {
+    super(props);
+    this.state = {
+      domain: "http://10.220.138.177:5000",
+      data: [
+        {
+          title: "IT",
+          esg: 90,
+          checked: false
+        },
+        {
+          title: "FINANCE",
+          esg: 85,
+          checked: false
+        },
+        {
+          title: "AGRICULTURE",
+          esg: 70,
+          checked: false
+        },
+        {
+          title: "REAL STATE",
+          esg: 60,
+          checked: false
+        },
+        {
+          title: "MANUFACTURE",
+          esg: 77,
+          checked: false
         }
-    }
+      ]
+    };
+  }
 
-    getData = () => {
+  requestData = (apiRoute) => {
+    var api = this.state.domain + apiRoute;
+    let tempData = [];
+    axios
+      .get(api)
+      .then(response => {
+        console.log(response);
+        response.data.sector_esg.foreach( (item) => {
+          tempData.push({
+            title: item[0],
+            esg: item[1].toFixed(2),
+            checked: false
+          })
+          this.setState({
+            data:tempData
+          })
+        } );
+      })
+      .catch(function(error) {
+        console.log(error);
+        console.log("access data fail");
+      });
+  };
 
-    }
+  componentDidMount() {
+    //this.requestData("/get_stock_info");
+    this.requestData("/get_sector");
+  }
 
-    componentDidMount(){
+  clickItem = e => {
+    let item = e.target;
+    let key = item.getAttribute("data-key");
+    if (key == null) item = item.parentNode;
+    key = item.getAttribute("data-key");
+    let tempData = this.state.data;
+    tempData[key].checked = !tempData[key].checked;
+    this.setState({
+      data: tempData
+    });
+  };
 
-    }
-    
-    clickItem = (e) => {
-        let item = e.target;
-        let key = item.getAttribute("data-key");
-        if( key==null ) item = item.parentNode;
-        key = item.getAttribute("data-key");
-        let tempData = this.state.data;
-        tempData[ key ].checked = !tempData[ key ].checked;
-        this.setState({
-            data: tempData
-        })
-    }
-
-    render() {
-        return (
-
-            <Row type="flex" justify="space-around">
-                {
-                    this.state.data.map( (item,key) => {
-                        return(
-                            <Col key={key}>
-                                <Button className={item.checked?"sector-item sector-item-on":"sector-item"} data-key={key} onClick = {this.clickItem}>
-                                    <b className="title">{item.title}</b><p>ESG Score: {item.esg}</p>
-                                </Button>
-                            </Col>
-                        )
-                    })
+  render() {
+    return (
+      <Row type="flex" justify="space-around">
+        {this.state.data.map((item, key) => {
+          return (
+            <Col key={key}>
+              <Button
+                className={
+                  item.checked ? "sector-item sector-item-on" : "sector-item"
                 }
-            </Row>
-        );
-    }
+                data-key={key}
+                onClick={this.clickItem}
+              >
+                <b className="sector-title">{item.title}</b>
+                <p>ESG Score: {item.esg}</p>
+              </Button>
+            </Col>
+          );
+        })}
+      </Row>
+    );
+  }
 }
 
 export default SelectSectors;
